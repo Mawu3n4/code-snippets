b16b833c7d04c4e09c1480f35d68152f2cd34c7d
HardCand01 <dibe.zack@gmail.com>
2014-06-30 23:10:08 +0200

    add: Fibonacci sequence

---
+++ Cpp/Metaprogramming/fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+template<int N>
+struct Fibonacci {
+  static const int value = Fibonacci<N-1>::value + Fibonacci<N-2>::value;
+};
+
+template<>
+struct Fibonacci<0> {
+  static const int value = 0;
+};
+
+template<>
+struct Fibonacci<1> {
+  static const int value = 1;
+};
+
+int main(void) {
+  std::cout << "Fib(5) = " << Fibonacci<5>::value << std::endl;
+  return (0);
+}
