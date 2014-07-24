a99b735e9e96e1cd9bedeb65d508f5f64fa18b6e
Mawu3n4 <zdibe@capensis.fr>
2014-07-24 13:55:20 +0200

    add: buy buttons

--- Mobile/Cordova/test-purchase/www/js/index.js
+++ Mobile/Cordova/test-purchase/www/js/index.js
@@ -1,3 +1,4 @@
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -31,7 +32,7 @@ var app = {
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
@@ -45,5 +46,58 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+
+        // start to initialize PayPalMobile library
+        app.initPaymentUI();
+    },
+    initPaymentUI : function () {
+        var clientIDs = {
+            "PayPalEnvironmentProduction": "YOUR_PRODUCTION_CLIENT_ID",
+            "PayPalEnvironmentSandbox": "YOUR_SANDBOX_CLIENT_ID"
+        };
+        PayPalMobile.init(clientIDs, app.onPayPalMobileInit);
+
+    },
+    onSuccesfulPayment : function(payment) {
+        console.log("payment success: " + JSON.stringify(payment, null, 4));
+    },
+    onFuturePaymentAuthorization : function(authorization) {
+        console.log("authorization: " + JSON.stringify(authorization, null, 4));
+    },
+    createPayment : function () {
+      // for simplicity use predefined amount
+        var paymentDetails = new PayPalPaymentDetails("1.50", "0.40", "0.05");
+        var payment = new PayPalPayment("1.95", "USD", "Awesome Sauce", "Sale", paymentDetails);
+        return payment;
+    },
+    configuration : function () {
+      // for more options see `paypal-mobile-js-helper.js`
+        var config = new PayPalConfiguration({merchantName: "My test shop", merchantPrivacyPolicyURL: "https://mytestshop.com/policy", merchantUserAgreementURL: "https://mytestshop.com/agreement"});
+        return config;
+    },
+    onPrepareRender : function() {
+      // buttons defined in index.html
+      //  <button id="buyNowBtn"> Buy Now !</button>
+      //  <button id="buyInFutureBtn"> Pay in Future !</button>
+        var buyNowBtn = document.getElementById("buyNowBtn");
+        var buyInFutureBtn = document.getElementById("buyInFutureBtn");
+
+        buyNowBtn.onclick = function(e) {
+        // single payment
+            PayPalMobile.renderSinglePaymentUI(app.createPayment(), app.onSuccesfulPayment, app.onUserCanceled);
+        };
+
+        buyInFutureBtn.onclick = function(e) {
+        // future payment
+            PayPalMobile.renderFuturePaymentUI(app.onFuturePaymentAuthorization, app.onUserCanceled);
+        };
+    },
+    onPayPalMobileInit : function() {
+      // must be called
+      // use PayPalEnvironmentNoNetwork mode to get look and feel of the flow
+        PayPalMobile.prepareToRender("PayPalEnvironmentNoNetwork", app.configuration(), app.onPrepareRender);
+    },
+    onUserCanceled : function(result) {
+        console.log(result);
     }
 };
