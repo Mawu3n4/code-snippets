b44239348eb17ffbfcb583b9bdb92ad723ce9ebf
Mawu3n4 <zdibe@capensis.fr>
2014-08-06 14:01:21 +0200

    add: dynamic generation

--- DailyProgrammer/174/174-Intermediate/Mawu3n4/solve.py
+++ DailyProgrammer/174/174-Intermediate/Mawu3n4/solve.py
@@ -8,24 +8,25 @@ seed = hashlib.sha256(username).hexdigest()
 seed += seed
 
 img = Image.new('RGB', (320, 320), "white")
+
 pen = ImageDraw.Draw(img)
 pixels = img.load()
 
-def genSquare(color, seed):
+def genSquare(color, seed, size):
     x = 0
     y = 0
     for c in seed:
-        if 52 < ord(c) < 100: pen.rectangle(((x, y), (x+20, y+20)), fill=color)
-        x = 0 if x >= 320/2 - 20 else x + 20
-        y = y + 20 if x == 0 else y
+        if 52 < ord(c) < 100: pen.rectangle(((x, y), (x+size, y+size)), fill=color)
+        x = 0 if x >= img.size[0]/2 - size else x + size
+        y = y + size if x == 0 else y
 
-genSquare((150, 75, 0), seed)
-genSquare((250, 250, 0), seed[::-1])
+genSquare((150, 75, 0), seed, 40)
+genSquare((250, 250, 0), seed[::-1], 40)
 
 # Vertical Symmetry
-for i in range(160):
-    for j in range(320):
-        pixels[160+i, j] = pixels[160-i, j]
+for i in range(img.size[0]/2):
+    for j in range(img.size[1]):
+        pixels[img.size[0]/2+i, j] = pixels[img.size[0]/2-i, j]
 
 img.save(username + "-out.png", "PNG")
 
