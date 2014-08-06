eabc361a0145965fe8c993263130d28e5961ed33
Mawu3n4 <zdibe@capensis.fr>
2014-08-06 13:12:01 +0200

    add: two colours

--- DailyProgrammer/174/174-Intermediate/Mawu3n4/solve.py
+++ DailyProgrammer/174/174-Intermediate/Mawu3n4/solve.py
@@ -11,15 +11,16 @@ img = Image.new('RGB', (320, 320), "white")
 pen = ImageDraw.Draw(img)
 pixels = img.load()
 
-x = 0
-y = 0
+def genSquare(color, seed):
+    x = 0
+    y = 0
+    for c in seed:
+        if 52 < ord(c) < 100: pen.rectangle(((x, y), (x+20, y+20)), fill=color)
+        x = 0 if x >= 320/2 - 20 else x + 20
+        y = y + 20 if x == 0 else y
 
-# Draw the squares
-for c in seed:
-    fill = (150, 75, 0) if 52 < ord(c) < 100 else (255, 255, 255)
-    pen.rectangle(((x, y), (x+20, y+20)), fill=fill)
-    x = 0 if x >= 320/2 - 20 else x + 20
-    y = y + 20 if x == 0 else y
+genSquare((150, 75, 0), seed)
+genSquare((250, 250, 0), seed[::-1])
 
 # Vertical Symmetry
 for i in range(160):
