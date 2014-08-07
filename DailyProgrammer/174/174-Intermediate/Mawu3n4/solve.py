b20da6b26900057a295dd2e3645f664602c29fcf
Mawu3n4 <zdibe@capensis.fr>
2014-08-07 12:15:29 +0200

    add: change color depending on sha hash

--- DailyProgrammer/174/174-Intermediate/Mawu3n4/solve.py
+++ DailyProgrammer/174/174-Intermediate/Mawu3n4/solve.py
@@ -20,8 +20,9 @@ def genSquare(color, seed, size):
         x = 0 if x >= img.size[0]/2 - size else x + size
         y = y + size if x == 0 else y
 
-genSquare((150, 75, 0), seed, 40)
-genSquare((250, 250, 0), seed[::-1], 40)
+color = sum([ord(c) for c in seed]) % 255
+genSquare((color, color, 0), seed, 40)
+genSquare((0, 255-color, 255-color), seed[::-1], 40)
 
 # Vertical Symmetry
 for i in range(img.size[0]/2):
