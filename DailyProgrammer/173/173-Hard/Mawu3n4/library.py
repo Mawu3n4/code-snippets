0bdd6bb07f8aaeeff12f3e086b86a4da7855cec0
Mawu3n4 <zdibe@capensis.fr>
2014-08-08 14:14:22 +0200

    add: refactored code, fixed combat

--- DailyProgrammer/173/173-Hard/Mawu3n4/library.py
+++ DailyProgrammer/173/173-Hard/Mawu3n4/library.py
@@ -88,7 +88,7 @@ rested_pool = {"You feel energy flow through your veins.\n",
                "A foe crossing your path right now would be a dead foe.\n",
                "You never felt so good before.\n"}
 
-hit_pool = {"The beast swing its arms at you and its claws gripps deeply into your flesh.\n"}
+hit_pool = {"The beast swings its arms at you and its claws gripps deeply into your flesh.\n"}
 
 monster_pos_pool = [
     "You can't hear the growling of the beast anymore, it is not a good thing.\n",
--- DailyProgrammer/173/173-Hard/Mawu3n4/solve.py
+++ DailyProgrammer/173/173-Hard/Mawu3n4/solve.py
@@ -3,23 +3,28 @@ import library
 import random
 
 
-def genNewPath(other_path={'length':0, 'obstacle': False, 'food': 0}, player=None):
-    if player:
-        teleport(genNewPath(other_path), player)
-    else:
-        return {
-            'length': abs(random.randrange(10,60,10) - other_path['length'] + 10),
-            'food': (random.randrange(0, 150, 20) if not other_path['food'] and
-                     random.randrange(0,100,10) < 40 else 0),
-            'obstacle': (not random.randrange(0,3) if not other_path['obstacle']
-                         else False)
-            }
+class Monster(object):
+    def __init__(self, *args, **kwargs):
+        super(Monster, self).__init__()
+        self.pos = kwargs.get('pos', 0)
+        self.speed = kwargs.get('speed', 20)
+        self.attack = kwargs.get('attack', 80)
+        self.acceleration = kwargs.get('acceleration', 20)
 
+    def update(self, player_dist):
+        # Print msg depending on the pos of the monster
+        library.printStrWithDelay(library.getPosMonsterSentence(
+                player_dist - self.pos))
 
-def teleport(path, player):
-    player.dist += path['length']
-    player.food += path['food']
+        # Its coming
+        self.pos += self.speed
+        # increase speed according to player.distance
+        self.speed += self.acceleration
 
+    def enrage(self, **kwargs):
+        self.pos = kwargs.get('pos', 0)
+        self.attack += kwargs.get('factor', 15)
+        self.speed = kwargs.get('reseted_speed', 20)
 
 # Peon: Peon
 # Warrior: Start with 50 more stamina
@@ -41,38 +46,128 @@ class Player(object):
 
     def checkStamina(self):
         if self.stamina <= 0:
-            library.printStrWithDelay("You made it as far as {0} miles but yu are exhausted and can't go further.\n Tonight, you will feed the beast.".format(self.dist))
-            exit
+            library.printStrWithDelay("You made it as far as {0}".format(self.dist))
+            library.printStrWithDelay(" miles but you are exhausted and can't go")
+            library.printStrWithDelay(" further.\nTonight, you will feed the beast.")
+            return False
+
+        return True
+
 
     def printStatus(self):
         library.printStrWithDelay("Stamina {0}, food left {1}\n".format(
                 self.stamina, self.food))
 
+
+    def rest(self):
+        library.printStrWithDelay("The hollowed tree looks comfortable, ")
+        library.printStrWithDelay("do you want to rest a bit ?..[Y/N] ")
+
+        u_input = raw_input()
+        self.resting = True if u_input[0] == "Y" else False
+
+        if self.resting:
+            self.stamina += 10 if self.food >= 10 else self.food
+            self.food -= 10 if self.food >= 10 else self.food
+            self.stamina += self.stamina * self.rest_rate
+            library.printStrWithDelay(library.getRestedSentence())
+
+
+    def move(self, choice, path):
+        library.printStrWithDelay(
+            "You went " + ("right.\n" if choice == 'R' else "left.\n"))
+
+        # Update ressources
+        self.dist += path['length']
+        self.food += path['food']
+        self.stamina -= path['length']
+
+        # Cross obstacles
+        if path['obstacle']:
+            library.printStrWithDelay("Something blocks your way.\n"
+                                      + library.getWalkedSentence())
+            self.stamina -= self.action_cost
+
+
+    def combat(self, opponent):
+        library.printStrWithDelay(
+            "You can't run anymore, take your {0} and fight !\n".format(
+                self.weapon))
+
+        res = random.randrange(0,2)
+        while not res:
+            library.printStrWithDelay(library.getMissedSentence())
+            self.stamina -= opponent.attack + self.action_cost
+            library.printStrWithDelay(library.getHitSentence())
+            if not self.checkStamina(): return False
+            res = random.randrange(0,3)
+
+        return True
+
+
 class Warrior(Player):
     def __init__(self, **kwargs):
-        Player.__init__(self, stamina=250, spec="Warrior", **kwargs)
+        Player.__init__(self, stamina=300, **kwargs)
 
 class Scout(Player):
     def __init__(self, **kwargs):
-        Player.__init__(self, spec="Scout", **kwargs)
+        Player.__init__(self, **kwargs)
 
         self.power = genNewPath
-        self.power_up = "Your sharp view notices the way behind the rock on your left and you vault your way through.\n"
+        self.power_up = "Your sharp view notices the way behind the rock "
+        self.power_up += "on your left and you vault your way through.\n"
 
 class Mage(Player):
     def __init__(self, **kwargs):
-        Player.__init__(self, spec="Mage", **kwargs)
+        Player.__init__(self, **kwargs)
 
         self.power = teleport
-        self.power_up = "You open up the book from your satchel and you close your eyes as the arcanes surrounds you, you re-open them and are throug the right path.\n"
+        self.power_up = "You open up the book from your satchel and you close "
+        self.power_up += "your eyes as the arcanes surrounds you, you re-open "
+        self.power_up += "them and are throug the right path.\n"
 
 class Cleric(Player):
     def __init__(self, **kwargs):
-        Player.__init__(self, spec="Cleric", rest_rate=0.2, **kwargs)
+        Player.__init__(self, rest_rate=0.2, **kwargs)
 
 class Archer(Player):
     def __init__(self, **kwargs):
-        Player.__init__(self, spec="Archer", action_cost=10, **kwargs)
+        Player.__init__(self, action_cost=10, **kwargs)
+
+
+def getNewPaths():
+    paths = {}
+
+#     library.printStrWithDelay(("The branches of the narrow trees slap your \
+# face as you try to stay alive and two paths emerge before you.\n"))
+
+    paths['R'] = genNewPath()
+    paths['L'] = genNewPath(paths['R'])
+
+    # printPath(paths['R'], "right")
+    # printPath(paths['L'], "left")
+
+    return paths
+
+
+def genNewPath(other_path={'length':0, 'obstacle': False, 'food': 0},
+               player=None):
+    if player:
+        teleport(genNewPath(other_path), player)
+    else:
+        return {
+            'length': abs(random.randrange(10,60,10) - other_path['length'] + 10),
+            'food': (random.randrange(0, 150, 20) if not other_path['food'] and
+                     random.randrange(0,100,10) < 40 else 0),
+            'obstacle': (not random.randrange(0,3) if not other_path['obstacle']
+                         else False)
+            }
+
+
+def teleport(path, player):
+    player.dist += path['length']
+    player.food += path['food']
+
 
 def initGame():
     options = {}
@@ -95,85 +190,39 @@ def printPath(path, direction):
             str(path['food']) + "lbs of " if path['food'] else "no ", direction))
 
 
-player = initGame()
-monster = {
-    'pos': 0,
-    'speed': 20,
-    'attack': 80
-    }
-paths = {}
+def gameLoop(player, monster):
+    while player.checkStamina():
 
-while True:
+        if not player.resting:
+            paths = getNewPaths()
 
-    if not player.resting:
-        library.printStrWithDelay(("The branches of the narrow trees slap your \
-face as you try to stay alive and two paths emerge before you.\n"))
+            # library.printStrWithDelay("Which path do you take ?.. [R/L" +
+            #                           ("/Power]" if player.power else "]"))
+            u_input = raw_input()
+            u_input = 'R' if not len(u_input) else u_input.capitalize()[0]
 
-        paths['R'] = genNewPath()
-        paths['L'] = genNewPath(paths['R'])
+            if u_input == 'P':
+                library.printStrWithDelay(player.power_up)
+                player.power(paths['R'], player)
 
-        printPath(paths['R'], "right")
-        printPath(paths['L'], "left")
+            else:
+                player.move(u_input, paths[u_input])
 
-        library.printStrWithDelay("Which path do you take ?.. [R/L" + "/Power]" if player.power else "]")
-        u_input = raw_input()
-        u_input = 'R' if not len(u_input) else u_input.capitalize()[0]
+        monster.update(player.dist)
 
-        if u_input == 'P':
-            library.printStrWithDelay(player.power_up)
-            player.power(paths['R'], player)
+        if monster.pos >= player.dist:
+            if not player.combat(monster):
+                return
 
-        else:
-            library.printStrWithDelay(
-                "You went " + ("right.\n" if u_input == 'R' else "left.\n"))
+            library.printStrWithDelay(library.getStrikedSentence())
+            library.printStrWithDelay("You managed to make it flee but it will")
+            library.printStrWithDelay(" come back, more adamant to feast on you.")
+            monster.enrage(factor=15, reseted_speed=20, pos=player.dist-20)
 
-            # Update ressources
-            player.dist += paths[u_input]['length']
-            player.food += paths[u_input]['food']
-            player.stamina -= paths[u_input]['length']
+        player.printStatus()
+        player.rest()
+        player.checkStamina()
 
-            # Cross obstacles
-            if paths[u_input]['obstacle']:
-                library.printStrWithDelay("Something blocks your way.\n"
-                                          + library.getWalkedSentence())
-                player.stamina -= player.action_cost
 
-    else:
-        player.stamina += 10 if player.food >= 10 else player.food
-        player.food -= 10 if player.food >= 10 else player.food
-        player.stamina += player.stamina * player.rest_rate
-        library.printStrWithDelay(library.getRestedSentence())
-
-    # Print msg depending on the pos of the monster
-    library.printStrWithDelay(library.getPosMonsterSentence(
-            player.dist - monster['pos']))
-
-    # Its coming
-    monster['pos'] += monster['speed']
-    # increase speed according to player.distance
-    monster['speed'] += 20
-
-    if monster['pos'] >= player.dist:
-        library.printStrWithDelay("You can't run anymore, take your {0} and fight !\n".format(player.weapon))
-        res = random.randrange(0,3)
-        while res:
-            library.printStrWithDelay(library.getMissedSentence())
-            player.stamina -= monster['attack'] + player.atack_cost
-            player.checkStamina()
-
-        library.printStrWithDelay(library.getStrikedSentence())
-        # Change distance according to player.distance
-        monster['pos'] = player.dist - 20
-        monster['attack'] += 15
-        monster['speed'] = 20
-
-    player.printStatus()
-    library.printStrWithDelay("The hollowed tree looks comfortable, do you want to rest a bit ?..[Y/N] ")
-    u_input = raw_input()
-    player.resting = True if u_input[0] == "Y" else False
-
-    player.checkStamina()
-
-## TODO
-#   Combat
-#   Increase speed
+# gameLoop(initGame(), Monster())
+gameLoop(Warrior(weapon="Axe", spec="Warrior"), Monster())
