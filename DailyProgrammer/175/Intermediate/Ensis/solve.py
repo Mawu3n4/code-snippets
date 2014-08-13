bbb999d79a1ad94d76c6d39283886d711f0144bd
Ensis <pierre.ecormier@yahoo.fr>
2014-08-13 23:29:00 +0200

    add 175-Intermediate

---
+++ DailyProgrammer/175/Intermediate/Ensis/solve.py
@@ -0,0 +1,20 @@
+import sys
+import re
+
+if len(sys.argv) < 3:
+    print "Usage: python solve.py 'word word' 'l e t t e r s'"
+    sys.exit()
+
+letters = "".join(sorted(sys.argv[2].split(" ")))
+msz = 0
+out = []
+
+for word in sys.argv[1].split(" "):
+    if re.search(".*".join(sorted(word)), letters):
+        if len(word) > msz:
+            msz = len(word)
+            out = []
+        if len(word) == msz:
+            out.append(word)
+
+print " ".join(out) if out else "No Words Found"
