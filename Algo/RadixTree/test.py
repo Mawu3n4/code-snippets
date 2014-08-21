8a4cb398dab4a07d84194a0330f416730cad6710
HardCand01 <dibe.zack@gmail.com>
2014-08-21 23:58:35 +0200

    fix: import

--- Algo/RadixTree/test.py
+++ Algo/RadixTree/test.py
@@ -1,27 +1,26 @@
 
-import utils.branch
-import utils.leaf
-
-import rtree
+from utils.branch import Branch
+from utils.leaf import Leaf
+from rtree.rtree import RadixTree
 
 
 ### TODO: Insert
-rootLeaf = utils.leaf.Leaf()
-tLeaf = utils.leaf.Leaf()
-slowLeaf = utils.leaf.Leaf()
-lyLeaf = utils.leaf.Leaf()
-estLeaf = utils.leaf.Leaf()
-oastLeaf = utils.leaf.Leaf()
-ingLeaf = utils.leaf.Leaf()
-erLeaf = utils.leaf.Leaf()
+rootLeaf = Leaf()
+tLeaf = Leaf()
+slowLeaf = Leaf()
+lyLeaf = Leaf()
+estLeaf = Leaf()
+oastLeaf = Leaf()
+ingLeaf = Leaf()
+erLeaf = Leaf()
 
-tBranch = utils.branch.Branch(label='t', leaf=tLeaf)
-slowBranch = utils.branch.Branch(label='slow', leaf=slowLeaf)
-lyBranch = utils.branch.Branch(label='ly', leaf=lyLeaf)
-estBranch = utils.branch.Branch(label='est', leaf=estLeaf)
-oastBranch = utils.branch.Branch(label='oast', leaf=oastLeaf)
-ingBranch = utils.branch.Branch(label='ing', leaf=ingLeaf)
-erBranch = utils.branch.Branch(label='er', leaf=erLeaf)
+tBranch = Branch(label='t', leaf=tLeaf)
+slowBranch = Branch(label='slow', leaf=slowLeaf)
+lyBranch = Branch(label='ly', leaf=lyLeaf)
+estBranch = Branch(label='est', leaf=estLeaf)
+oastBranch = Branch(label='oast', leaf=oastLeaf)
+ingBranch = Branch(label='ing', leaf=ingLeaf)
+erBranch = Branch(label='er', leaf=erLeaf)
 
 rootLeaf.branches = [tBranch, slowBranch]
 tLeaf.branches = [oastBranch, estBranch]
@@ -29,7 +28,7 @@ slowLeaf.branches = [lyBranch]
 oastLeaf.branches = [ingBranch, erBranch]
 ### TODO
 
-tree = rtree.RadixTree(root=rootLeaf)
+tree = RadixTree(root=rootLeaf)
 print "Enter strings to lookup: "
 while (1):
     print tree.lookup(raw_input())
