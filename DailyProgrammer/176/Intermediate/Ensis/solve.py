a76ac665d4db36fb645974fb5413bbafbe1cc113
Ensis <pierre.ecormier@yahoo.fr>
2014-08-21 14:55:01 +0200

    add: end of 176-inter

--- DailyProgrammer/176/Intermediate/Ensis/solve.py
+++ DailyProgrammer/176/Intermediate/Ensis/solve.py
@@ -1,4 +1,20 @@
+from operator import __add__, __sub__, __mul__, __div__, __pow__
 import re
+
+def __builtin_sum(cells, select):
+    s = 0.0
+    for c in select:
+        s += parseExp(cells, getCell(cells, c))
+    return s
+
+def __builtin_product(cells, select):
+    s = 1.0
+    for c in select:
+        s *= parseExp(cells, getCell(cells, c))
+    return s
+
+def __builtin_average(cells, select):
+    return __builtin_sum(cells, select) / len(select)
 
 def colRowToXY(s):
     gr = re.match("([A-Z]+)([0-9]+)", s)
@@ -47,22 +63,41 @@ def getCell(cells, xy):
         cells[xy] = "0"
     return cells[xy]
 
-def setCell(cells, xy, val):
-    cells[xy] = val
-    return val
+def testRegex(rx, f, ops, funcs):
+    m = re.match(rx, f)
+    if not m:
+        return False
+    gr = m.groups()
+    return funcs[ops.index(gr[1])](parseExp(cells, gr[0]), parseExp(cells, gr[2]))
 
-def parseExp(cells, xy):
-    return getCell(cells, xy)
+def parseExp(cells, f):
+    if re.match("[0-9]+(\.[0-9]+)?$", f):
+        return float(f)
+    elif re.match("[A-Z]+[0-9]+$", f):
+        return parseExp(cells, getCell(cells, colRowToXY(f)))
+    m = re.match("([a-z]+)\(([^\)]+)\)$", f)
+    if m:
+        if "__builtin_" + m.group(1) in globals():
+            return globals()["__builtin_" + m.group(1)](cells, selectCells(m.group(2)))
+        else:
+            print "Unknown builtin: " + m.group(1)
+            return 0
+    m = testRegex("(.+)([+-])(.+)$", f, "+-", [__add__, __sub__])
+    if m is False:
+        m = testRegex("(.+)([*/])(.+)$", f, "*/", [__mul__, __div__])
+    if m is False:
+        m = testRegex("(.+)([\^])(.+)$", f, "^", [__pow__])
+    return (m)
 
 def applyCommand(cells):
     f = raw_input()
     eq = f.split("=")
     if len(eq) == 1:
-        print parseExp(cells, colRowToXY(eq[0]))
+        print parseExp(cells, getCell(cells, colRowToXY(eq[0])))
     else:
         select = selectCells(eq[0])
         for c in select:
-            setCell(cells, c, eq[1])
+            cells[c] = eq[1]
 
 cells = {}
 while True:
