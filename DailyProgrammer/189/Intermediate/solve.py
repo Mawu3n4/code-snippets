f554fb104fe224029115a27b5dd515113f4dd01b
Zack <dibe.zack@gmail.com>
2014-11-26 17:54:01 +0100

    add: dec2rom

--- DailyProgrammer/189/Intermediate/solve.py
+++ DailyProgrammer/189/Intermediate/solve.py
@@ -7,9 +7,21 @@ roman = {'I': 1,
          'D': 500,
          'M': 1000}
 
-u_input = raw_input('Enter numeral number: ')
+dec = {1: 'I',
+       4: 'IV',
+       5: 'V',
+       9: 'IX',
+       10: 'X',
+       40: 'XL',
+       50: 'L',
+       90: 'XC',
+       100: 'C',
+       400: 'CD',
+       500: 'D',
+       900: 'CM',
+       1000: 'M'}
 
-print [roman[x] for x in u_input][:-1]
+u_input = raw_input('Enter numeral number: ')
 
 if not set(roman.keys()) >= set(u_input):
     print 'Wrong number, refer to:'
@@ -21,6 +33,11 @@ else:
                 for i, num in enumerate(s[:-1])], roman[s[-1]])
 
     def convert_dec2rom(nb):
-        return ""
+        res = ''
+        for dec2rom in reversed(dec.keys()):
+            while nb >= dec2rom:
+                res += dec[dec2rom]
+                nb -= dec2rom
+        return res
 
-    print convert_rom2dec(u_input)
+    print convert_dec2rom(convert_rom2dec(u_input))
