97167789836bc61efb2bb470ed71658a89b813f3
Mawu3n4 <dibe.zack@gmail.com>
2015-01-12 22:11:12 +0000

    add: crossover and mutation + precision

--- Algo/Genetic/findResult.py
+++ Algo/Genetic/findResult.py
@@ -3,12 +3,10 @@ from rcdtype import *
 import operator
 import math
 
-# TARGET = int(raw_input("Number to find ?: "))
-# MAX_GEN = int(raw_input("Maximum number of generations ?: "))
+TARGET = int(raw_input("Number to find ?: "))
+MAX_GEN = int(raw_input("Maximum number of generations ?: "))
 
-TARGET = 42.0
 POP_SIZE = 50
-MAX_GEN = 300
 GEN_LEN = 4
 CHROM_LEN = 80
 MUTATION_RATE = 0.001
@@ -23,14 +21,16 @@ DECODE_OP = {10: (operator.add, '+'),
              13: (operator.div, '/')}
 
 def rand():
-    from random import randint
-    return randint(0,9)
+    from random import random
+    return random()
 
 def parseGenes(genes):
     op = True
     literals = []
 
     for i in range(0, GEN_LEN * CHROM_LEN, GEN_LEN):
+        if not genes[i:i+GEN_LEN]:
+            continue
         curr_gene = int(''.join(genes[i:i+GEN_LEN]), 2)
         if op:
             if curr_gene < 10 or curr_gene > 13:
@@ -72,12 +72,34 @@ def printChromosome(chrom):
     print ''
 
 def randGenes():
-    return ['1' if rand() < 5 else '0' for x in range(GEN_LEN * CHROM_LEN)]
+    return ['1' if rand() < 0.5 else '0' for x in range(GEN_LEN * CHROM_LEN)]
 
 def randChromosome():
     chrom = Chromosome(randGenes(), 0.0)
     chrom.fitness = getFitness(chrom.genes, TARGET)
     return chrom
+
+def crossover(pick1, pick2):
+    if rand() < CROSSOVER_RATE:
+        cross = int(rand() * GEN_LEN * CHROM_LEN)
+        return (Chromosome(pick1.genes[0:cross] + pick2.genes[cross:GEN_LEN * CHROM_LEN], 0.0),
+                Chromosome(pick2.genes[0:cross] + pick1.genes[cross:GEN_LEN * CHROM_LEN], 0.0))
+    return pick1, pick2
+
+def mutate(chrom):
+    chrom.genes = ['1' if gene == '0' else '0' for gene in chrom.genes]
+    return chrom
+
+def wheelPick(pop, total_fitness):
+    rand_part = rand() * total_fitness
+    fitness = 0.0
+
+    for i in xrange(POP_SIZE):
+        fitness += pop[i].fitness
+        if fitness >= rand_part:
+            return pop[i]
+
+    return Chromosome([], 0.0)
 
 def main():
     pop = [Chromosome(randGenes(), 0.0) for i in xrange(POP_SIZE)]
@@ -96,14 +118,17 @@ def main():
         if found or n_gen > MAX_GEN:
             break
 
-        # TODO
-        # next_pop = []
-        # total_fitness = sum(chrom.fitness for chrom in pop)
+        next_pop = []
+        total_fitness = sum(chrom.fitness for chrom in pop)
 
-        # for i in xrange(POP_SIZE):
-        #     next_pop.append(crossover(wheelPick(pop, total_fitness),
-        #                               wheelPick(pop, total_fitness)))
-        # pop = next_pop
+        for i in xrange(POP_SIZE):
+            pick1 = wheelPick(pop, total_fitness)
+            pick2 = wheelPick(pop, total_fitness)
+            pick1, pick2 = crossover(pick1, pick2)
+            next_pop.append(mutate(pick1))
+            next_pop.append(mutate(pick2))
+
+        pop = next_pop
 
         n_gen += 1
 
