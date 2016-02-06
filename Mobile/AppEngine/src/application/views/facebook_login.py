bef4e2a716863b851e2d449afd3bee49c1bbff2c
Mawu3n4 <dibe.zack@gmail.com>
2016-02-06 19:48:31 +0000

    add: get secret keys from conf

--- Mobile/AppEngine/src/application/views/facebook_login.py
+++ Mobile/AppEngine/src/application/views/facebook_login.py
@@ -6,11 +6,6 @@ from application import app
 
 __all__ = ['facebook_login', 'facebook_authorized', 'logout']
 
-FACEBOOK_APP_ID = ''
-FACEBOOK_APP_SECRET = ''
-
-app.config["SECRET_KEY"] = FACEBOOK_APP_SECRET
-
 oauth = OAuth()
 
 facebook = oauth.remote_app('facebook',
@@ -18,8 +13,8 @@ facebook = oauth.remote_app('facebook',
     request_token_url=None,
     access_token_url='/oauth/access_token',
     authorize_url='https://www.facebook.com/dialog/oauth',
-    consumer_key=FACEBOOK_APP_ID,
-    consumer_secret=FACEBOOK_APP_SECRET,
+    consumer_key=app.config['FACEBOOK_APP_ID'],
+    consumer_secret=app.config['FACEBOOK_APP_SECRET'],
     request_token_params={'scope': ('email, ')}
 )
 
@@ -31,14 +26,14 @@ def pop_login_session():
     session.pop('logged_in', None)
     session.pop('facebook_token', None)
 
-@app.route("/facebook_login")
+@app.route('/facebook_login')
 def facebook_login():
     return facebook.authorize(
         callback=url_for('facebook_authorized',
                          next=request.args.get('next'),
                          _external=True))
 
-@app.route("/facebook_authorized")
+@app.route('/facebook_authorized')
 @facebook.authorized_handler
 def facebook_authorized(resp):
     next_url = request.args.get('next') or url_for('index')
@@ -48,9 +43,9 @@ def facebook_authorized(resp):
     session['logged_in'] = True
     session['facebook_token'] = (resp['access_token'], '')
 
-    return redirect(next_url)
+    return redirect('/friends')
 
-@app.route("/logout")
+@app.route('/logout')
 def logout():
     pop_login_session()
     return redirect(url_for('index'))
