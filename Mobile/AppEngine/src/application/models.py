db54abb721ea4d6a0d986ec1af027070b591d596
Mawu3n4 <dibe.zack@gmail.com>
2016-02-07 18:56:33 +0000

    add: token in user model

--- Mobile/AppEngine/src/application/models.py
+++ Mobile/AppEngine/src/application/models.py
@@ -3,3 +3,4 @@ from google.appengine.ext import ndb
 class User(ndb.Model):
     name = ndb.StringProperty()
     friends = ndb.KeyProperty(kind="User", repeated=True)
+    token = ndb.StringProperty()
